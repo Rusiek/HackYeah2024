7b1ead6dc0f5bfe82719b1aaec1834db6e907bdf
Jakub Worek <worekkuba@gmail.com>
2024-09-28 22:30:13 +0200

    all paths endpoint

--- backend/server/app/routes/paths/paths.py
+++ backend/server/app/routes/paths/paths.py
@@ -5,39 +5,29 @@ import os
 import ast
 
 bp_paths = Blueprint('paths', __name__)
+file_path = os.path.join(os.path.dirname(__file__), 'velo.txt')
 
 @bp_paths.route('/', methods=['GET'])
 def get_path():
-  #open csv file and map it to json
-  paths_array = [[
-    [19.907462, 50.067538],
-    [19.907236, 50.066823],
-    [19.913411, 50.065849],
-    [19.919037, 50.064511],
-    [19.923522, 50.063847,]
-  ], [
-    [19.906894, 50.068029],
-    [19.907437, 50.069503],
-    [19.910575, 50.069032],
-    [19.911385, 50.068852],
-    [19.912821, 50.068238],
-    [19.912638, 50.068086]
-  ]]
+  with open(file_path, 'r', encoding='utf-8') as file:
+    content = file.read().strip()
+
+  data = ast.literal_eval(content)
+
+  all_paths = [[list(x) for x in array] for array in data]
 
   response_dict = {}
-  response_dict['paths'] = paths_array
+  response_dict['paths'] = all_paths
 
-
+    # Use ast.literal_eval for safe evaluation
   return jsonify(response_dict)
-
-file_path = os.path.join(os.path.dirname(__file__), 'velo.txt')
 
 @bp_paths.route('/<id>', methods=['GET'])
 def get_path_by_id(id):
   with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read().strip()
+    content = file.read().strip()
 
-    # Use ast.literal_eval for safe evaluation
+  # Use ast.literal_eval for safe evaluation
   data = ast.literal_eval(content)
   
   array = data[int(id)]
