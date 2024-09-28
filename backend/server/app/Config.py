526aa7364c7083ff0f408512166f18a631ca426b
Massny <pawel.fornagiel2@gmail.com>
2024-09-28 20:21:55 +0200

    update config with HERE api values

--- backend/server/app/Config.py
+++ backend/server/app/Config.py
@@ -5,3 +5,5 @@ class Config:
   DEBUG = True
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is-secret'
   REDIS_URL = 'redis://localhost:6379/0'
+  HERE_API_FLOW_URL = 'https://data.traffic.hereapi.com/v7/flow'
+  HERE_API_KEY = os.environ.get('HERE_API_KEY') or ''
