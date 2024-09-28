303d79ed59ad012f2845330e629e53bcc5f8b4f6
Massny <pawel.fornagiel2@gmail.com>
2024-09-29 00:29:23 +0200

    change default testing route

--- backend/server/app/routes/routes.py
+++ backend/server/app/routes/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, current_app
+from flask import Blueprint
 from app import redis_client
 import redis
 
@@ -7,15 +7,8 @@ bp_routes = Blueprint('routes', __name__)
 # test route - delete later
 @bp_routes.route('/')
 def index():
-  redis_client.set('mykey', 'Hello, Redis!')
-  value = redis_client.get('mykey')
-  return f'The value of mykey is: {value.decode("utf-8")}'
-
-@bp_routes.route('/ping')
-def ping_redis():
-    try:
-        redis_client.ping()
-        return 'Connected to Redis!'
-    except redis.exceptions.ConnectionError:
-        return 'Failed to connect to Redis.'
-
+  try:
+    redis_client.ping()
+    return 'Connected to Redis!'
+  except redis.exceptions.ConnectionError:
+      return 'Failed to connect to Redis.'
