daec7dfa237a4ff5044702e44838617dfe0caacb
Jakub Worek <worekkuba@gmail.com>
2024-09-28 16:18:35 +0200

    accidents api

--- backend/server/app/__init__.py
+++ backend/server/app/__init__.py
@@ -10,13 +10,18 @@ redis_client = FlaskRedis()
 
 # blueprints
 from app.routes.routes import bp_routes
+from app.routes.accidents.accidents import bp_accidents
 
 def create_app(config=Config):
   app = Flask(__name__)
   app.config.from_object(config)
   redis_client.init_app(app)
   
-  app.register_blueprint(bp_routes)
+  app.register_blueprint(bp_routes, url_prefix='/test')
+  app.register_blueprint(bp_accidents, url_prefix='/accidents')
 
+  @app.route('/')
+  def index():
+    return 'Hello, World!'
   
   return app
---
+++ backend/server/app/routes/accidents/accidents.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, request, jsonify
+import requests
+import json
+import csv
+import os
+
+bp_accidents = Blueprint('accidents', __name__)
+
+@bp_accidents.route('/', methods=['GET'])
+def get_accidents():
+  #open csv file and map it to json
+  path_to_csv = os.path.join(os.path.dirname(__file__), 'accidents.csv')
+  data = []
+  with open(path_to_csv, 'r', encoding='utf-8') as f:
+    reader = csv.DictReader(f)
+    rows = list(reader)
+    for row in rows:
+      data.append(row)
+
+  return jsonify(data)
