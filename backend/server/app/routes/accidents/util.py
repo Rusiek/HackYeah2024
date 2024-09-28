ac6f938f690844ca9422d7c417eb2de3ee6c581b
Massny <pawel.fornagiel2@gmail.com>
2024-09-29 00:19:06 +0200

    move accidents util file

--- backend/server/app/routes/accidents/util.py
+++ backend/server/app/routes/accidents/util.py
@@ -2,8 +2,8 @@ import csv
 import os
 import json
 
-path_to_csv = os.path.join(os.path.dirname(__file__), 'accidents.csv')
-path_to_json = os.path.join(os.path.dirname(__file__), 'accidents.json')
+path_to_csv = os.path.join(os.path.dirname(__file__), 'data/accidents.csv')
+path_to_json = os.path.join(os.path.dirname(__file__), 'data/accidents.json')
 data = []
 with open(path_to_csv, 'r', encoding='utf-8') as f:
   reader = csv.DictReader(f)
